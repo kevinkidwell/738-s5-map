4d14fb75f8689f718047e98a06f3d0b451c9c55b
Kevin Kidwell <kekidwell@gmail.com>
2025-11-30 12:48:07 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles.css';
 
-createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
