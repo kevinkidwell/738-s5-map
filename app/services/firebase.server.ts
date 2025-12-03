19196f9ee90095b55e379dcf7371aa1d6b25cd3f
Kevin Kidwell <kekidwell@gmail.com>
2025-12-02 19:41:18 -0500

    Update firebase.server.ts

--- app/services/firebase.server.ts
+++ app/services/firebase.server.ts
@@ -2,15 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-console.log("✅ Firebase env check:", {
-  apiKey: !!process.env.FIREBASE_API_KEY,
-  authDomain: !!process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: !!process.env.FIREBASE_PROJECT_ID,
-  storageBucket: !!process.env.FIREBASE_STORAGE_BUCKET,
-  senderId: !!process.env.FIREBASE_SENDER_ID,
-  appId: !!process.env.FIREBASE_APP_ID,
-});
-
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY!,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
