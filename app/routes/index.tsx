d7ddb967b18f5f95b0b4761a771f97a5bc06be43
Kevin Kidwell <kekidwell@gmail.com>
2025-12-02 21:11:12 -0500

    fix index

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,26 @@
-import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
 
+import AllianceManager from "../components/AllianceManager";
+import { getAlliances, updateAllianceShade, addAlliance } from "../services/alliances.server";
+
+// Loader fetches initial alliances from Firestore
 export const loader = async () => {
-  return json({ message: "Alliance Manager is live!" });
+  const alliances = await getAlliances();
+  return json({ alliances });
 };
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
-  return <h1>{data.message}</h1>;
+  const { alliances } = useLoaderData<typeof loader>();
+
+  return (
+    <div className="container py-4">
+      <h1 className="mb-4">Alliance Manager</h1>
+      <AllianceManager
+        alliances={alliances}
+        onAddAlliance={addAlliance}
+        onEditShade={updateAllianceShade}
+      />
+    </div>
+  );
 }
