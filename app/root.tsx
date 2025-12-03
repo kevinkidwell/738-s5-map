5880966d7e160a0053f0a1969f0090df6dc2ec33
Kevin Kidwell <kekidwell@gmail.com>
2025-12-02 19:46:05 -0500

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -8,10 +8,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export const meta: MetaFunction = () => ([
-  { title: "Alliance Manager" },
-  { name: "viewport", content: "width=device-width, initial-scale=1" },
-]);
+export const meta: MetaFunction = () => {
+  return [{ title: "Alliance Manager" }];
+};
 
 export const links: LinksFunction = () => ([
   { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" },
