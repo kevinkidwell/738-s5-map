080a72a00c5a071db8307b4246a10d25846e92c0
Kevin Kidwell <kekidwell@gmail.com>
2025-12-02 21:50:53 -0500

    Update AllianceManager.tsx

--- app/components/AllianceManager.tsx
+++ app/components/AllianceManager.tsx
@@ -1,19 +1,50 @@
+import { useEffect, useState } from "react";
 import AllianceForm from "./AllianceForm";
 import AllianceTable from "./AllianceTable";
+import {
+  addAlliance,
+  updateAllianceShade,
+  subscribeAlliances,
+} from "../services/alliances.server";
 
-export default function AllianceManager({
-  alliances,
-  onAddAlliance,
-  onEditShade,
-}: {
-  alliances: { id: string; name: string; shades: string[] }[];
-  onAddAlliance: (name: string, shades: string[]) => Promise<void>;
-  onEditShade: (id: string, index: number, newColor: string) => Promise<void>;
-}) {
+export type Alliance = { id: string; name: string; shades: string[] };
+
+type AllianceManagerProps = {
+  readOnly?: boolean;
+};
+
+export default function AllianceManager({ readOnly = false }: AllianceManagerProps) {
+  const [alliances, setAlliances] = useState<Alliance[]>([]);
+
+  useEffect(() => {
+    const unsubscribe = subscribeAlliances(setAlliances);
+    return () => unsubscribe();
+  }, []);
+
+  const handleAddAlliance = async (name: string, shades: string[]) => {
+    if (!readOnly) {
+      await addAlliance(name, shades);
+    }
+  };
+
+  const handleEditShade = async (id: string, index: number, newColor: string) => {
+    if (!readOnly) {
+      await updateAllianceShade(id, index, newColor);
+    }
+  };
+
   return (
-    <div>
-      <AllianceForm onSubmit={onAddAlliance} />
-      <AllianceTable alliances={alliances} onEditShade={onEditShade} />
+    <div className="container py-4">
+      <h1 className="h3 mb-4">Alliance Manager</h1>
+
+      {/* Hide form in read-only mode */}
+      {!readOnly && <AllianceForm onSubmit={handleAddAlliance} />}
+
+      <AllianceTable
+        alliances={alliances}
+        onEditShade={handleEditShade}
+        readOnly={readOnly}
+      />
     </div>
   );
 }
